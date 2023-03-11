45b9bd6457d1b268690c5384490528278b58786c
miguel <miguelpositivo02@gmail.com>
2023-03-10 21:50:42 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Thread. Sleep(1000);
+Console.WriteLine("Preparar");
+Thread. Sleep(1000);
+Console.WriteLine("Apontar");
+Thread. Sleep(1000);
+Console.WriteLine("FOGO!");
